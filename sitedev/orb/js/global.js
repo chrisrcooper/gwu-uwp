6b85c9a2c42f9aa5d80897b478ac8cac81fc5c45
Christopher R. Cooper <chrisynd@gmail.com>
2011-03-07 10:47:45 -0500

    Full update

--- sitedev/orb/js/global.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function()
-{
-	$('form:not(.filter) :input:visible:first').focus();
-	
-	$('select#periods').change(function(){
-		var link = $('select#periods option:selected').val();
-		window.location=link;
-	});
-});
