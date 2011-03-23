289d613f9f2aae21abd6da6ab92644d05a2ba9e6
Christopher R. Cooper <chrisynd@gmail.com>
2011-03-23 10:01:33 -0400

    Alumni mobile sliding functionality

--- sitedev-dev/mobile/alumni/js/script.js
+++ sitedev-dev/mobile/alumni/js/script.js
@@ -8,6 +8,38 @@ $(document).ready(function()
 		$(this).next().slideToggle('slow');
 		$(this).find('.icon').html('-');
 	});
+	
+	function fbFetch(){
+	  //Set Url of JSON data from the facebook graph api. make sure callback is set with a '?' to overcome the cross domain problems with JSON
+      var url = "http://graph.facebook.com/GWAlumni/feed?limit=1&callback=?";
+		
+		//Use jQuery getJSON method to fetch the data from the url and then create our unordered list with the relevant data.
+		$.getJSON(url,function(json){
+		    var html = "<p><a href='http://www.facebook.com/GWAlumni'>GWAlumni</a> </strong>";
+				//loop through and within data array's retrieve the message variable.
+		    	$.each(json.data,function(i,fb){		      		
+		      		
+		      		fb.message = fb.message.substr(0,140);
+		      		fb.message += '...';
+		      		html += fb.message; 
+		    	});
+		    html += "</p>";
+
+			//A little animation once fetched
+			/*$('#facebook').animate({opacity:0}, 500, function()
+			{*/
+					$('#facebook').html(html);
+			/*});
+
+		    $('#facebook').animate({opacity:1}, 500);*/
+		});
+	};
+	fbFetch();
+
+	
+	$(function(){
+		$('#tweets').tweetable({username: 'GWAlumni', time: false, limit: 1, replies: true, position: 'append'});
+	});
 });
 
 
